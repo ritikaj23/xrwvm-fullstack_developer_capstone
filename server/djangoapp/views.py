676342492985a844b1fc769a41cfecf0d53ab4b9
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2024-01-02 19:32:30 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -128,7 +128,8 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            return JsonResponse({"status": 401, "message": f"Error in posting review: {e}"})
+            return JsonResponse({"status": 401, 
+                                 "message": f"Error in posting review: {e}"})
         finally:
             print("add_review request successful!")
     else:
@@ -143,5 +144,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
