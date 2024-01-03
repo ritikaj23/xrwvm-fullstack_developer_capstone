972144303bacbbc83bdfb35a2786cc181d01e3e6
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2024-01-03 12:27:43 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS =\
-    ['localhost','https://ritikaj-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    ['localhost', 
+     'https://ritikaj-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS =\
     ['https://ritikaj-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
@@ -93,7 +94,7 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS =\
+AUTH_PASSWORD_VALIDATORS =
 [
     {
         'NAME':
