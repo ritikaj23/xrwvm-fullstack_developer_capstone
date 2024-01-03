f4c18d79a271f061d9aa46cb3c38eedda8b2c42b
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2024-01-03 13:18:50 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -144,7 +144,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-    cars.append({"CarModel": car_model.name,
-                 "CarMake": car_model.car_make.name})
-# Delete the extra spaces at the end of the lines
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
+        # Delete the extra spaces at the end of the lines
 return JsonResponse({"CarModels": cars})
